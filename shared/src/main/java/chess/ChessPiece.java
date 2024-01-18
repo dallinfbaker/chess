9dad52de8ef14785ef4d195aaccf3c2b36faa802
dallinfbaker <jedidallin@gmail.com>
2024-01-18 10:48:27 -0700

    implement pawn moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,11 +1,6 @@
 package chess;
 
-import chess.PieceMovesCalculator.PieceMovesCalculator;
-import chess.PieceMovesCalculator.PieceMovesCalculator.*;
-
-
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.*;
 
 /**
  * Represents a single chess piece
@@ -52,18 +47,16 @@ public class ChessPiece {
     public PieceType getPieceType() {
         return this.type;
     }
-
     private void addMove(ChessPosition position) {
         ChessMove move = new ChessMove(this.position, position, null);
         this.moves.add(move);
     }
-    private void addMove(int row, int col) {
-        ChessPosition endPosition = new ChessPosition(row, col);
-        ChessMove move = new ChessMove(this.position, position, null);
+    private void addMove(ChessPosition position, PieceType promotion) {
+        ChessMove move = new ChessMove(this.position, position, promotion);
         this.moves.add(move);
     }
     private boolean CheckPosition(int row, int column) {
-        if (row > 8 || row < 1 || column > 8 || column < 1) { return true; }
+        if ((row > 8 || row < 1) || (column > 8 || column < 1)) { return true; }
         ChessPosition cur = new ChessPosition(row, column);
         if (this.board.getPiece(cur) == null) {
             this.addMove(cur);
@@ -79,27 +72,46 @@ public class ChessPiece {
     }
 
     /**
-     * Calculate all of the positions a chess piece can move to
-     * @return Collection of valid moves
+     * Calculate all positions a chess piece can move to
      */
-    private Collection<ChessMove> kingMoves() {
-        throw new RuntimeException("Not implemented");
+    private void kingMoves() {
+        CheckPosition(this.position.getRow() + 1, this.position.getColumn() + 1);
+        CheckPosition(this.position.getRow() + 1, this.position.getColumn());
+        CheckPosition(this.position.getRow() + 1, this.position.getColumn() - 1);
+        CheckPosition(this.position.getRow(), this.position.getColumn() + 1);
+        CheckPosition(this.position.getRow(), this.position.getColumn() - 1);
+        CheckPosition(this.position.getRow() - 1, this.position.getColumn() + 1);
+        CheckPosition(this.position.getRow() - 1, this.position.getColumn());
+        CheckPosition(this.position.getRow() - 1, this.position.getColumn() - 1);
     }
     private void queenMoves() {
         this.bishopMoves();
         this.rookMoves();
     }
     private void bishopMoves() {
-        boolean north = false, south = false, east = false, west = false;
-        for (int i = 1; i < 9; ++i) {
-            if(!north) north = CheckPosition(this.position.getRow() + i, this.position.getColumn() + i);
-            if(!south) south = CheckPosition(this.position.getRow() + i, this.position.getColumn() - i);
-            if(!east) east = CheckPosition(this.position.getRow() - i, this.position.getColumn() + i);
-            if(!west) west = CheckPosition(this.position.getRow() - i, this.position.getColumn() - i);
+        for (int i = 1; i < 9 - this.position.getRow(); ++i) {
+            if (CheckPosition(this.position.getRow() + i, this.position.getColumn() + i)) break;
+        }
+        for (int i = 1; i < this.position.getRow(); ++i) {
+            if (CheckPosition(this.position.getRow() + i, this.position.getColumn() - i)) break;
+        }
+        for (int i = 1; i < 9 - this.position.getColumn(); ++i) {
+            if (CheckPosition(this.position.getRow() - i, this.position.getColumn() + i)) break;
+        }
+        for (int i = 1; i < this.position.getColumn(); ++i) {
+            if (CheckPosition(this.position.getRow() - i, this.position.getColumn() - i)) break;
         }
     }
-    private Collection<ChessMove> knightMoves() {
-        throw new RuntimeException("Not implemented");
+    private void knightMoves() {
+        int i = 1, j = 2;
+        KnightSet(j, i);
+        KnightSet(i, j);
+    }
+    private void KnightSet(int i, int j) {
+        CheckPosition(this.position.getRow() + j, this.position.getColumn() + i);
+        CheckPosition(this.position.getRow() + j, this.position.getColumn() - i);
+        CheckPosition(this.position.getRow() - j, this.position.getColumn() + i);
+        CheckPosition(this.position.getRow() - j, this.position.getColumn() - i);
     }
     private void rookMoves() {
         for (int i = this.position.getRow() + 1; i < 9; ++i) {
@@ -115,16 +127,23 @@ public class ChessPiece {
             if (CheckPosition(this.position.getRow(), i)) break;
         }
     }
+    private void pawnPromotions() {
+        PieceType[] pieces = new PieceType[]{PieceType.ROOK, PieceType.BISHOP, PieceType.KNIGHT, PieceType.QUEEN};
+        ChessMove[] movesArray = this.moves.toArray(new ChessMove[0]);
+        this.moves.clear();
+        for (ChessMove move : movesArray) {
+            for (PieceType type : pieces) {
+                this.addMove(move.getEndPosition(), type);
+            }
+        }
+    }
     private void pawnMoves() {
         int direction = this.color == ChessGame.TeamColor.WHITE ? 1 : -1;
         int endRow = this.color == ChessGame.TeamColor.WHITE ? 8 : 1;
         int StartRow = this.color == ChessGame.TeamColor.WHITE ? 2 : 7;
-
-        if (this.position.getRow() == StartRow) {
-            CheckPosition(this.position.getRow() + 2, this.position.getColumn());
-        }
-        if (this.position.getRow() < endRow) {
-            ChessPosition cur = new ChessPosition(this.position.getRow() + direction, this.position.getColumn());
+        ChessPosition cur;
+        if (this.position.getRow() * direction < endRow * direction) {
+            cur = new ChessPosition(this.position.getRow() + direction, this.position.getColumn());
             if (this.board.getPiece(cur) == null) {
                 this.addMove(cur);
                 cur = new ChessPosition(this.position.getRow() + direction * 2, this.position.getColumn());
@@ -134,12 +153,17 @@ public class ChessPiece {
             }
             cur = new ChessPosition(this.position.getRow() + direction, this.position.getColumn() + 1);
             if (this.board.getPiece(cur) != null) {
-                if (this.board.getPiece(cur).getTeamColor() != this.color) { this.addMove(cur); }
+                if (this.board.getPiece(cur).getTeamColor() != this.color) {
+                    this.addMove(cur);
+                }
             }
             cur = new ChessPosition(this.position.getRow() + direction, this.position.getColumn() - 1);
             if (this.board.getPiece(cur) != null) {
-                if (this.board.getPiece(cur).getTeamColor() != this.color) { this.addMove(cur); }
+                if (this.board.getPiece(cur).getTeamColor() != this.color) {
+                    this.addMove(cur);
+                }
             }
+            if (this.position.getRow() == endRow - direction) pawnPromotions();
         }
     }
 
@@ -149,15 +173,26 @@ public class ChessPiece {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         this.board = board;
         this.position = myPosition;
-//        PieceMovesCalculator calculator;
-        this.moves = new ArrayList<ChessMove>();
+        this.moves = new HashSet<>();
         switch(this.type) {
-            case KING: kingMoves();
-            case QUEEN: queenMoves();
-            case BISHOP: bishopMoves();
-            case KNIGHT: knightMoves();
-            case ROOK: rookMoves();
-            case PAWN: pawnMoves();
+            case KING:
+                kingMoves();
+                break;
+            case QUEEN:
+                queenMoves();
+                break;
+            case BISHOP:
+                bishopMoves();
+                break;
+            case KNIGHT:
+                knightMoves();
+                break;
+            case ROOK:
+                rookMoves();
+                break;
+            case PAWN:
+                pawnMoves();
+                break;
         }
         return this.moves;
     }
@@ -171,11 +206,4 @@ public class ChessPiece {
         ChessPiece piece = (ChessPiece) o;
         return this.color == piece.color & this.type == piece.type;
     }
-/*    *//**
-     * @return Hash of position
-     *//*
-    @Override
-    public int hashCode() {
-        throw new RuntimeException("Not implemented");
-    }*/
 }
