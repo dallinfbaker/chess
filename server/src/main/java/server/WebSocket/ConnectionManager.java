350e7d4580daa845d20a1d38b12980154382a6c2
dallinfbaker <jedidallin@gmail.com>
2024-04-03 17:08:25 -0600

    clean code

--- server/src/main/java/server/WebSocket/ConnectionManager.java
+++ server/src/main/java/server/WebSocket/ConnectionManager.java
@@ -20,17 +20,11 @@ public class ConnectionManager {
     public void broadcast(int id, String ignoredAuth, ServerMessage msg) throws IOException {
         HashMap<String, Session> sessions = connections.get(id);
         Session session;
-//        ServerMessage msg;
         for (String auth : sessions.keySet()) {
             if (Objects.equals(ignoredAuth, auth)) continue;
             session = sessions.get(auth);
             if (Objects.isNull(session)) sessions.remove(auth);
             else if (session.isOpen()) {
-//                msg = switch (type) {
-//                    case NOTIFICATION -> new NotificationMessage(auth, message);
-//                    case LOAD_GAME -> new LoadGameMessage();
-//                    case ERROR -> new ErrorMessage(auth, message);
-//                }
                 msg.setAuthToken(auth);
                 send(session, msg);
             }
@@ -42,7 +36,5 @@ public class ConnectionManager {
     public void send(Session session, ServerMessage msg) {
         try { session.getRemote().sendString(new Gson().toJson(msg)); }
         catch (IOException e) { throw new RuntimeException(e); }
-
-//        session.getRemote().send(new Gson().toJson(msg));
     }
 }
